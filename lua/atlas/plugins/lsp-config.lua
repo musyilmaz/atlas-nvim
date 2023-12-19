1133e7e6a56845c9f71c9cdda93e450d7dfaae2e
Mustafa Yilmaz <musyilmaz.dev@gmail.com>
2023-12-20 02:13:31 +0300

    Lsp seems like working

--- lua/atlas/plugins/lsp-config.lua
+++ lua/atlas/plugins/lsp-config.lua
@@ -16,44 +16,15 @@ return {
       local on_attach = function(_, bufnr)
         opts.buffer = bufnr
 
-        opts.desc = "Show documentation for what is under cursor"
-        keymap.set("n", "K", vim.lsp.buf.hover, opts)
-
-        opts.desc = "See available code actions"
-        keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-
-        opts.desc = "Smart rename"
-        keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-
-        opts.desc = "Go to previous diagnostic"
-        keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>", opts)
-
-        opts.desc = "Go to next diagnostic"
-        keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>", opts)
-
-        opts.desc = "Show LSP references"
-        keymap.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
-
-        opts.desc = "Go to declaration"
-        keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-
-        opts.desc = "Show LSP definitions"
-        keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
-
-        opts.desc = "Show LSP implementations"
-        keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
-
-        opts.desc = "Show LSP type definitions"
-        keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
-
-
-        opts.desc = "Show buffer diagnostics"
-        keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
-
-
-
-        opts.desc = "Restart LSP"
-        keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)
+        keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+        keymap.set("n", "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+        keymap.set("n", "<leader>gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+        keymap.set("n", "<leader>go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+        keymap.set("n", "<leader>gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+        keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+        keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+        keymap.set("n", "<leader>d[", "<cmd>lua vim.diagnostic.goto_prev()<cr>", opts)
+        keymap.set("n", "<leader>d]", "<cmd>lua vim.diagnostic.goto_next()<cr>", opts)
       end
 
       local capabilities = cmp_nvim_lsp.default_capabilities()
