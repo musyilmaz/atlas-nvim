96a15e0368689659429642590bcab17acbd1114d
Mustafa Yilmaz <musyilmaz.dev@gmail.com>
2024-05-31 14:08:57 +0300

    new trouble settings

--- lua/atlas/plugins/trouble.lua
+++ lua/atlas/plugins/trouble.lua
@@ -2,58 +2,38 @@ return {
   {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      local trouble = require("trouble")
-
-      trouble.setup({
-        position = "bottom",
-        height = 10,
-        width = 50,
-        icons = true,
-        mode = "workspace_diagnostics",
-        fold_open = "",
-        fold_closed = "",
-        group = true,
-        padding = true,
-        action_keys = {
-          close = "q",
-          cancel = "<esc>",
-          refresh = "r",
-          jump = { "<cr>", "<tab>" },
-          jump_close = { "o" },
-          open_split = { "<c-x>" },
-          open_vsplit = { "<c-v>" },
-          open_tab = { "<c-t>" },
-          toggle_mode = "m",
-          toggle_preview = "P",
-          hover = "K",
-          preview = "p",
-          close_folds = { "zM", "zm" },
-          open_folds = { "zR", "zr" },
-          toggle_fold = { "zA", "za" },
-          previous = "k",
-          next = "j",
-        },
-        indent_lines = true,
-        auto_open = false,
-        auto_close = false,
-        auto_preview = true,
-        auto_fold = false,
-        auto_jump = { "lsp_definitions" },
-        signs = {
-          error = "",
-          warning = "",
-          hint = "",
-          information = "",
-          other = "﫠",
-        },
-        use_diagnostic_signs = false,
-      })
-
-      local keymap = vim.keymap
-
-      keymap.set("n", "<leader>tt", "<cmd>Trouble diagnostics toggle<cr>")
-      keymap.set("n", "<leader>td", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>")
-    end,
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
+    opts = {},
   },
 }
