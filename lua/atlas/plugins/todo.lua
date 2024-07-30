99c5fb7fa6a5e3a2d1615fd3dfe0c53fec40b37a
Mustafa Yilmaz <musyilmaz.dev@gmail.com>
2024-07-30 12:53:52 +0300

    add trouble and telescope for todo

--- lua/atlas/plugins/todo.lua
+++ lua/atlas/plugins/todo.lua
@@ -1,5 +1,13 @@
 return {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
+  keys = {
+    {
+      "<leader>tc",
+      "<cmd>Trouble todo<cr>",
+      desc = "Todo (Trouble)",
+    },
+    { "<leader>tv", "<cmd>TodoTelescope<cr>", desc = "Todo (Telescope)" },
+  },
   opts = {},
 }
