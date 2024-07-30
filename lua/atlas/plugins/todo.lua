97666894ef8f81f83847b10e0215367a69a06c2b
Mustafa Yilmaz <musyilmaz.dev@gmail.com>
2024-07-30 13:02:41 +0300

    change keybindings of todo

--- lua/atlas/plugins/todo.lua
+++ lua/atlas/plugins/todo.lua
@@ -2,12 +2,8 @@ return {
   "folke/todo-comments.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
   keys = {
-    {
-      "<leader>tc",
-      "<cmd>Trouble todo<cr>",
-      desc = "Todo (Trouble)",
-    },
-    { "<leader>tv", "<cmd>TodoTelescope<cr>", desc = "Todo (Telescope)" },
+    { "<leader>ht", "<cmd>Trouble todo<cr>", desc = "Todo (Trouble)" },
+    { "<leader>hs", "<cmd>TodoTelescope<cr>", desc = "Todo (Telescope)" },
   },
   opts = {},
 }
